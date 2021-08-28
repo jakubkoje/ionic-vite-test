56e73befd14bd6c67159437db9aaf6afed78309a
Jakub Jelínek <jakubkoje@gmail.com>
2021-08-28 23:44:22 +0200

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Hello World</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <img alt="Vue logo" src="/src/assets/logo.png" />
+            <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts" setup>
+    import HelloWorld from '/src/components/HelloWorld.vue'
+    import { IonPage, IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/vue';
+</script>
